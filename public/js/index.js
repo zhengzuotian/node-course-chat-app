81c4628d34b9dca207c49033c010c06081c47112
Simon Zheng <zhengzt@gmail.com>
2018-02-24 22:08:11 -0500

    add form for messages

--- public/js/index.js
+++ public/js/index.js
@@ -13,6 +13,32 @@ socket.on('disconnect', function(){
   console.log('Disconnected from server');
 });
 
-socket.on('newMessage', function(email){
-  console.log('New email', email);
+socket.on('newMessage', function(message){
+  console.log('New message', message);
+  var li = jQuery('<li></li>');
+  li.text((`${message.from}: ${message.text}`));
+
+  jQuery('#messages').append(li);
+});
+
+
+// socket.emit('createMessage', {
+//   from: 'Frank',
+//   text: 'Hi'
+// }, function(data){
+//   console.log('Got it', data);
+// });
+
+jQuery('#message-form').on('submit', function (e) {
+  e.preventDefault();
+  var text = jQuery('[name=message]').val()
+  socket.emit('createMessage', {
+    from: 'User',
+    text
+  }, function(){
+    var li = jQuery('<li></li>');
+    li.text((`User: ${text}`));
+
+    jQuery('#messages').append(li);
+  });
 });
--- server/server.js
+++ server/server.js
@@ -20,7 +20,7 @@ io.on('connection', (socket) => {
 
   socket.broadcast.emit('newMessage', generateMessage('Admin', 'New user joined!'));
 
-  socket.on('createMessage', (message) => {
+  socket.on('createMessage', (message, callback) => {
     console.log('createMessage', message);
     // io.emit('newMessage', {
     //   from: message.from,
@@ -28,6 +28,7 @@ io.on('connection', (socket) => {
     //   createdAt: new Date().getTime()
     // });
     socket.broadcast.emit('newMessage', generateMessage(message.from, message.text));
+    callback('This is from the server');
   });
 
   socket.on('disconnect', () => {
